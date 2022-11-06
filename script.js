12b48fab16f25af7207e1cf89f93f34a6bb8afd4
Astha Negi <asthanegi539@gmail.com>
2022-11-06 19:42:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,60 @@
 document.getElementById("inp").readOnly = true;
 
 let inpScrn = document.getElementById("inp");
+var stack = [];
+buttons = document.querySelectorAll(".btns");
 
-buttons = Array.from(document.querySelectorAll(".btns"));
 let iniVal = "";
 
 for(val of buttons) {
     val.addEventListener("click", (e)=>{
         ans = e.target.innerText;
-        if(ans == "X"){
+        if(ans == "x"){
             ans = "*";
             iniVal += ans;
             inpScrn.value = iniVal;
         }
-        else if(ans == "AC"){
+        else if(ans=="÷"){
+            ans="/";
+            iniVal += ans;
+            inpScrn.value = iniVal;
+        }
+        else if(ans == "clear"){
             iniVal = "";
             inpScrn.value = iniVal;
         }
-        else if(ans == "="){
-            iniVal = eval(iniVal);
-            inpScrn.value = iniVal;
+        else if(ans=="del"){
+            iniVal = inpScrn.value;
+            inpScrn.value = iniVal.substr(0,iniVal.length-1);
+        }
+        else if(ans == "ENTER"){
+            console.log(inpScrn.value);
+            rt = "√";
+            sec = inpScrn.value.substr(0,1);
+            if(_.isEqual(rt,sec)){
+                inpScrn.value = Math.sqrt(inpScrn.value.substr(1,iniVal.length));
+            }
+            else{
+                try{
+                    iniVal = eval(iniVal);
+                    inpScrn.value = iniVal;
+                }
+                catch(e){
+                    alert("Invalid Input");
+                }
+            }
+            stack.push(iniVal);
+        }
+        else if(ans == "∓"){
+            iniVal = inpScrn.value;
+            min = "-";
+            st = "(";
+            ed = ")";
+            inpScrn.value = st+min+iniVal+ed;
+        }
+        else if(ans=="ans"){
+            inpScrn.value = stack.pop();
+            stack.push(inpScrn.value);
         }
         else{
             iniVal += ans;
